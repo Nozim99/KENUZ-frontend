214c7ec1b83ea5d4e0a3fefa7d16421188e18d83
Nozimbek <mezes3758@gmail.com>
2025-02-12 12:13:16 +0500

    changed url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://kenuz.vercel.app';  // GLOBAL
+export const BASE_URL = 'http://service.mezes.uz/api';  // GLOBAL
 // export const BASE_URL = 'http://localhost:5000/api';  // LOCAL
 
-export const PUBLIC_URL = 'kenuz.vercel.app';  // GLOBAL
+export const PUBLIC_URL = 'https://kenuz.vercel.app';  // GLOBAL
 // export const PUBLIC_URL = 'http://localhost:3000';  // LOCAL
