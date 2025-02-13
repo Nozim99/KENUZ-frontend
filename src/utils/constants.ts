9845c633cb560ea18a9a09559e6c61897ed16b9e
Nozimbek <mezes3758@gmail.com>
2025-02-13 11:52:10 +0500

    http -> https

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://service.mezes.uz/api';  // GLOBAL
+export const BASE_URL = 'https://service.mezes.uz/api';  // GLOBAL
 // export const BASE_URL = 'http://localhost:5000/api';  // LOCAL
 
 export const PUBLIC_URL = 'https://kenuz.vercel.app';  // GLOBAL
