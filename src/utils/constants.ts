04ada1446f28ea604d19cef77b38ba3f5032436e
Nozimbek <mezes3758@gmail.com>
2025-02-20 20:35:09 +0500

    changed PUBLIC_URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://service.mezes.uz/api';  // GLOBAL
 // export const BASE_URL = 'http://localhost:5000/api';  // LOCAL
 
-export const PUBLIC_URL = 'https://kenuz.mezes.uz';  // GLOBAL
+export const PUBLIC_URL = 'https://kenuz.uz';  // GLOBAL
 // export const PUBLIC_URL = 'http://localhost:3000';  // LOCAL
